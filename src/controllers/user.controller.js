8f5c33202be93dad8b53a891ef80bd587bbc528c
prashlesh <iamprashlesh@gmail.com>
2025-11-19 20:53:25 +0530

    Created MongoDB Pipeline

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -98,7 +98,7 @@ const registerUser = asyncHandler(async(req,res)=>{
 
 
 
-}  )
+})
 
  const loginUser = asyncHandler(async(req,res)=>{
     // req body -> data
@@ -228,7 +228,7 @@ const updateAccountDetails = asyncHandler(async(req,res)=>{
 
     return res
     .status(200)
-    .json(new ApiResponse(200, "Account Details updated successfully"))
+    .json(new ApiResponse(200,{}, "Account Details updated successfully"))
 })
 
 const updateUserAvatar = asyncHandler(async(req,res) =>{
@@ -287,7 +287,80 @@ const updateUserCoverImage = asyncHandler(async(req,res) =>{
     )
 })
 
+const getUserChannelProfile = asyncHandler(async(req,res)=>{
+    const {username} = req.params
 
+    if (!username?.trim()) {
+        throw new ApiError(400, "Username is missing")
+
+    }
+
+   const channel = await User.aggregate([
+    {
+        $match:{
+            username:username?.toLowerCase()
+        }
+    },
+    {
+        $lookup:{
+            from:"subscriptions",
+            localField:"_id",
+            foreignField: "channel",
+            as: "subscribers"
+
+        }
+    },
+    {
+        $lookup:{
+            from:"subscriptions",
+            localField:"_id",
+            foreignField: "subscriber",
+            as: "subscribedTo"
+        }
+    },
+    {
+        $addFields:{
+            subscribersCount:{
+                $size:"$subscribers"
+            },
+            channelIsSubscribedToCount:{
+                $size: "$subscribedTo"
+            },
+            isSubscribed:{
+                $cond:{
+                    if:{$in: [req.user?._id,"subscribers.subscriber"]},
+                    then: true,
+                    else: false
+                }
+            }
+        }
+    },
+    {
+        $project:{
+            fullName : 1,
+            username:1,
+            subscribersCount: 1,
+            channelIsSubscribedToCount: 1,
+            isSubscribed: 1,
+            avatar: 1,
+            coverImage:1,
+            email:1
+
+        }
+    }
+   ])
+
+   if (!channel?.length) {
+    throw new ApiError(404,"channel does not exist")
+    
+   }
+
+   return res
+   .status(200)
+   .json(
+    new ApiResponse(200,channel[0], " User channel fetched successfully")
+   )
+})
 export {
     registerUser ,
     loginUser,
@@ -301,3 +374,5 @@ export {
 
 
 }
+
+
